06f4810d347587be10969568294bc336d7c2f12f
3r0ck-Ry4n <brock.ryan2@gmail.com>
2018-01-18 10:00:36 -0500

    Create TextCase.java
    Java class for text case conversion.  This class accesses the system clipboard to provide quick, easy, & user-friendly text case conversion.

---
+++ TextCase.java
@@ -0,0 +1,82 @@
+package textHandling;
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+
+public class TextCase {
+
+	public TextCase() {
+		
+	}
+	
+	public void clipToLower() {
+		
+		try {
+			Clipboard clipBoard = Toolkit.getDefaultToolkit().getSystemClipboard();
+			
+			String clip = ((String)clipBoard.getData(DataFlavor.stringFlavor)).toLowerCase();
+			
+			StringSelection stringSelection = new StringSelection(clip);
+			
+			clipBoard.setContents(stringSelection, null);
+		}
+		
+		catch(Exception e) {
+			
+		}
+	}
+	
+	public void clipToUpper() {
+		
+		try {
+			Clipboard clipBoard = Toolkit.getDefaultToolkit().getSystemClipboard();
+			
+			String clip = ((String)clipBoard.getData(DataFlavor.stringFlavor)).toUpperCase();
+			
+			StringSelection stringSelection = new StringSelection(clip);
+			
+			clipBoard.setContents(stringSelection, null);
+		}
+		
+		catch(Exception e) {
+			
+		}
+	}
+	
+	public void clipToTitleCase() {
+		
+		try {
+			StringBuilder titleCase = new StringBuilder();
+			
+			System.out.println("Title Case: " + titleCase + "\n");
+			
+			Clipboard clipBoard = Toolkit.getDefaultToolkit().getSystemClipboard();
+			String input = ((String)clipBoard.getData(DataFlavor.stringFlavor)).toLowerCase();
+			
+			System.out.println("input (from clipBoard): " + input + "\n");
+	    
+			boolean nextTitleCase = true;
+
+			for (char c : input.toCharArray()) {
+				if (Character.isSpaceChar(c)) {
+					nextTitleCase = true;
+				} else if (nextTitleCase) {
+					c = Character.toTitleCase(c);
+					nextTitleCase = false;
+				}
+
+				titleCase.append(c);
+			}
+						
+			StringSelection stringSelection = new StringSelection(titleCase.toString());
+			
+			clipBoard.setContents(stringSelection, null);
+		}
+		
+		catch(Exception e) {
+			
+		}
+	}
+}
